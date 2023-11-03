0ea92409add40f4c1d9416b02007d3c9b74bf8f4
juciley.costa <juciley.costa@e-unicred.com.br>
2023-11-03 17:27:08 -0300

    Resolução dos desafios.

--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -8,6 +8,11 @@ public class Question3 {
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
-
+    public Mono<Void> userIsValid(final User user){
+        return Mono.just(user).handle((u, sink) -> {
+            if(u.password().length() < 8){
+                sink.error(new Exception("Usuário Inválido"));
+            }
+       });
+    }
 }
